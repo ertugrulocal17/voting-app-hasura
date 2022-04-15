bddfc461648442c64a320b8909ce6a8a175542d2
ertugrulocal17 <ertugrulocal17@gmail.com>
2022-04-15 10:08:50 +0300

    created-new-voting

--- client/src/pages/New/index.js
+++ client/src/pages/New/index.js
@@ -1,9 +1,66 @@
-import React from "react";
-
+import { React, useState } from "react";
+import { useMutation } from "@apollo/client";
+import { NEW_QUESTION_MUTATION } from "./queries";
+const initalOptions = [{ title: "" }, { title: "" }];
 function NewQuestion() {
+  const [addQuestion, { loading, data }] = useMutation(NEW_QUESTION_MUTATION);
+
+  const [title, setTitle] = useState("");
+  const [options, setOptions] = useState(initalOptions);
+  const handleChangeOption = ({ target }) => {
+    const newArray = options;
+    newArray[target.id].title = target.value;
+
+    setOptions([...newArray]);
+  };
+  const handleSave = () => {
+    const filletOptions = options.filter((option) => option.title !== "");
+    if (title === "" || filletOptions.length < 2) return false;
+    addQuestion({
+      variables: {
+        input: {
+          title,
+          options: {
+            data: filletOptions,
+          },
+        },
+      },
+    });
+    setTitle("");
+    setOptions(initalOptions);
+  };
   return (
     <div>
-      <h1>NewQuestion</h1>
+      <h2>Questions</h2>
+      <input
+        placeholder="Type your question..."
+        value={title}
+        onChange={({ target }) => setTitle(target.value)}
+        disabled={loading}
+      />
+      <h2>Options</h2>
+      {options.map((option, index) => (
+        <div key={index}>
+          <input
+            placeholder="Type your option..."
+            value={option.title}
+            id={index}
+            onChange={handleChangeOption}
+            disabled={loading}
+          />
+        </div>
+      ))}
+
+      <button
+        disabled={loading}
+        onClick={() => setOptions([...options, { title: "" }])}
+      >
+        New Option
+      </button>
+
+      <button disabled={loading} onClick={handleSave}>
+        Save
+      </button>
     </div>
   );
 }
